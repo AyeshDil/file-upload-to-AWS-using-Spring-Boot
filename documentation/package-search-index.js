460edad39004a0280c8093d89036ba9a8d2a2267
AyeshDil <dilshanwma@gmail.com>
2023-01-27 06:04:57 +0530

    Finalized simple file upload app to the AWS S3 bucket.

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.myproject.fileupload.fileupload"},{"l":"com.myproject.fileupload.fileupload.config"},{"l":"com.myproject.fileupload.fileupload.controller"},{"l":"com.myproject.fileupload.fileupload.service"},{"l":"com.myproject.fileupload.fileupload.service.impl"}]
